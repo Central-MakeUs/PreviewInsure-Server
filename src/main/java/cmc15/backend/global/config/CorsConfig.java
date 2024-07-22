7146edc7368a55a2a2bc7c9058a980cc83bf5af6
Hyun Woo <hwsa10041@gmail.com>
2024-07-22 12:46:22 +0900

    feat : Cors 허용 주소 추가

--- src/main/java/cmc15/backend/global/config/CorsConfig.java
+++ src/main/java/cmc15/backend/global/config/CorsConfig.java
@@ -1,34 +1,24 @@
 package cmc15.backend.global.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
+public class CorsConfig implements WebMvcConfigurer {
 
-        config.addAllowedOrigin("http://localhost:8080");
-        config.addAllowedOrigin("http://localhost:8080/");
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("http://localhost:3000/");
-
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PATCH");
-        config.addAllowedMethod("DELETE");
-        config.setAllowCredentials(true);
-
-        config.addAllowedHeader("*");
-
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://api.preview.co.kr",
+                        "https://api.moidot.co.kr/",
+                        "https://www.moidot.co.kr",
+                        "https://www.moidot.co.kr/"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
-
 }
