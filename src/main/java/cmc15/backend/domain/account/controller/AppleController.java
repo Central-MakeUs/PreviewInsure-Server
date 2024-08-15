b13c9f42bb98a841248fc65b2817a56a06755bd3
Hyun Woo <hwsa10041@gmail.com>
2024-08-15 18:36:20 +0900

    feat : 애플 로그인 시 redirect 되도록 변경

--- src/main/java/cmc15/backend/domain/account/controller/AppleController.java
+++ src/main/java/cmc15/backend/domain/account/controller/AppleController.java
@@ -4,9 +4,12 @@ import cmc15.backend.domain.account.response.AccountResponse;
 import cmc15.backend.domain.account.service.AccountService;
 import cmc15.backend.global.CustomResponseEntity;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,12 +18,13 @@ public class AppleController {
     private final AccountService accountService;
 
     /**
-     * @apiNote 애플 로그인 API
      * @param request
      * @return AccountResponse.OAuthConnection
+     * @apiNote 애플 로그인 API
      */
     @PostMapping("/apple/token")
-    public CustomResponseEntity<AccountResponse.OAuthConnection> appleLogin(HttpServletRequest request) {
-        return CustomResponseEntity.success(accountService.getAppleInfo(request.getParameter("code")));
+    public void appleLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        CustomResponseEntity<AccountResponse.OAuthConnection> code = CustomResponseEntity.success(accountService.getAppleInfo(request.getParameter("code")));
+        response.sendRedirect("https://preview-insure-web-git-dev-sehuns-projects.vercel.app/callback/apple?token=" + code.getData().getAtk());
     }
 }
