22ebd86364141373c8e8e1ca10a37b5989285e04
Hyun Woo <hwsa10041@gmail.com>
2024-08-17 18:29:29 +0900

    feat : 보험회사에 따른 이미지 노출되도록 변경

--- src/main/java/cmc15/backend/domain/insurance/response/InsuranceResponse.java
+++ src/main/java/cmc15/backend/domain/insurance/response/InsuranceResponse.java
@@ -53,21 +53,61 @@ public class InsuranceResponse {
         private String link;
 
         public static Recommend to(final Insurance insurance, final String gender) {
-
-            String insuranceImage = "";
-            if (insurance.getInsuranceCompany().equals("삼성생명")) {
-                insuranceImage = "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%89%E1%85%A1%E1%86%B7%E1%84%89%E1%85%A5%E1%86%BC%E1%84%92%E1%85%AA%E1%84%8C%E1%85%A2.png";
-            }
-
             return Recommend.builder()
                     .insuranceId(insurance.getInsuranceId())
-                    .insuranceImage(insuranceImage)
+                    .insuranceImage(parseInsuranceImage(insurance))
                     .insuranceCompany(insurance.getInsuranceCompany())
                     .insuranceContent(insurance.getInsuranceContent())
                     .insuranceRate(insurance.getInsuranceRate())
                     .price((gender.equals("M")) ? insurance.getMalePrice() : insurance.getFemalePrice())
                     .link(insurance.getLink())
                     .build();
+        }
+
+        private static String parseInsuranceImage(Insurance insurance) {
+            String insuranceImage = switch (insurance.getInsuranceCompany()) {
+                case "삼성생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%89%E1%85%A1%E1%86%B7%E1%84%89%E1%85%A5%E1%86%BC%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "ABL생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/ABL%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "AIA생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/AIA%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "BNP파리바카디프생명" -> "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/BNP.png";
+                case "DB생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/DB%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "iM라이프" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/iM%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%91%E1%85%B3.png";
+                case "KB라이프생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/KB%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC%E1%84%87%E1%85%A9%E1%84%92%E1%85%A5%E1%86%B7.png";
+                case "KDB생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/KDB%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "NH농협생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/NH%E1%84%82%E1%85%A9%E1%86%BC%E1%84%92%E1%85%A7%E1%86%B8%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "교보라이프플래닛생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%80%E1%85%AD%E1%84%87%E1%85%A9%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%91%E1%85%B3%E1%84%91%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A2%E1%84%82%E1%85%B5%E1%86%BA%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "교보생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%80%E1%85%AD%E1%84%87%E1%85%A9%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "동양생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%83%E1%85%A9%E1%86%BC%E1%84%8B%E1%85%A3%E1%86%BC%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "라이나생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%82%E1%85%A1%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "메트라이프생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%86%E1%85%B5%E1%84%83%E1%85%B3%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%91%E1%85%B3.png";
+                case "미래에셋생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%86%E1%85%B5%E1%84%85%E1%85%A2%E1%84%8B%E1%85%A6%E1%84%89%E1%85%A6%E1%86%BA%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "신한라이프생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%89%E1%85%B5%E1%86%AB%E1%84%92%E1%85%A1%E1%86%AB%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%91%E1%85%B3.png";
+                case "푸본현대생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%91%E1%85%AE%E1%84%87%E1%85%A9%E1%86%AB%E1%84%92%E1%85%A7%E1%86%AB%E1%84%83%E1%85%A2%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "하나생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%92%E1%85%A1%E1%84%82%E1%85%A1%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "한화생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%92%E1%85%A1%E1%86%AB%E1%84%92%E1%85%AA%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                case "흥국생명" ->
+                        "https://reviewinsue-bucket.s3.ap-northeast-2.amazonaws.com/image/%E1%84%92%E1%85%B3%E1%86%BC%E1%84%80%E1%85%AE%E1%86%A8%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC.png";
+                default -> "";
+            };
+            return insuranceImage;
         }
     }
 }
